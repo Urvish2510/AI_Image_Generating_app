5ee575ba281a1c15658c70d77df724d05b024e52
Urvish2510 <uvpatel2510@gmail.com>
2023-01-30 15:48:36 +0530

    Started and configure folder structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import { Card, Loader, FormField } from "../components";
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className="max-w-7x1 mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
