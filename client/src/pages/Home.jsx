518dd079b952406b752c069c3d30b21655f3f9a6
Urvish2510 <uvpatel2510@gmail.com>
2023-02-09 09:57:28 +0530

    backend url integreted

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:8080/api/v1/post",
+        "https://ai-image-tl3k.onrender.com/api/v1/post",
         {
           method: "GET",
           headers: {
